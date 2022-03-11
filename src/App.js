36413592c09d7145b0f82ae287fec4fb50cc04b8
Lucian Nguyen <lucian.n@seznam.cz>
2022-03-11 11:58:24 +0100

    fetched and displayed arrival and departure time

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { DateTime } from 'luxon';
 
 function App() {
-  const [flights, getFlights] = useState([]);
+  const [flights, setFlights] = useState([]);
+  // const [departureTime, setDepartureTime] = useState(0);
+  // const [arrivalTime, setArrivalTime] = useState(0);
+
+
   const url =
     "https://api.skypicker.com/flights?flyFrom=PRG&to=VLC&partner=data4youcbp202106";
 
@@ -14,9 +19,30 @@ function App() {
     const resp = await fetch(url);
     const result = await resp.json();
     console.log(result);
-    getFlights(result);
+    setFlights(result.data);
   };
-  return <div className="App"></div>;
+  return <div className="App">
+    {flights.map((item, i) => (
+      <div key={i}>
+        <h2>
+          {item.flyFrom}
+        </h2>
+        <h3>
+          Departure time:
+        </h3>
+        <p>{DateTime.fromMillis(item.dTime * 1000).toFormat('hh:mm')}</p>
+
+        <h2>
+          {item.flyTo}
+        </h2>
+        <h3>
+          Arrival time:
+        </h3>
+        <p>{DateTime.fromMillis(item.aTime * 1000).toFormat('hh:mm')}</p>
+      </div>
+    ))}
+  
+  </div>;
 }
 
 export default App;
