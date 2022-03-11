16f261ea726c09bd1dafa0a40bc4cb780e65fb1d
ekincbc <ekincbc@gmail.com>
2022-03-11 12:10:54 +0100

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Flight from "./components/Flight";
+import Flights from "./components/Flights";
 
-function App() {
-  const [flights, getFlights] = useState([]);
-  const url =
-    "https://api.skypicker.com/flights?flyFrom=PRG&to=VLC&partner=data4youcbp202106";
-
-  useEffect(() => {
-    fetchFlightData();
-  }, []);
-
-  const fetchFlightData = async () => {
-    const resp = await fetch(url);
-    const result = await resp.json();
-    console.log(result);
-    getFlights(result);
-  };
-  return <div className="App"></div>;
-}
+function App() {}
 
 export default App;
---
+++ src/components/Flight.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Flights from "./Flights";
+
+function Flight({ flight }) {
+  const [showFlight, setShowFlight] = useState(false);
+  const [flyTo, flyFrom, id] = flight;
+
+  return (
+    
+  );
+}
+
+export default Flight;
